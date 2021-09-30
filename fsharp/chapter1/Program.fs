c01ccb6310653e01a90b0cd8a1236cb4547223f5
Mark Shevchenko <markshevchenko@gmail.com>
2021-09-30 08:03:22 +0300

    Solve exercise 1.1 in F#

---
+++ fsharp/chapter1/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+// 1.1
+let containsDublicates s =
+    s |> Seq.sort
+      |> Seq.pairwise
+      |> Seq.exists (fun pair -> fst pair = snd pair)
+
+[<EntryPoint>]
+let main argv =
+    // 1.1
+    assert not (containsDublicates "")
+    assert not (containsDublicates "abcdef")
+    assert containsDublicates "foo"
+    
+    0
