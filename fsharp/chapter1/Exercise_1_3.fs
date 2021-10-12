9e6164c2f0dfaa253406270d43c66bd19c46abac
Mark Shevchenko <markshevchenko@gmail.com>
2021-10-12 21:57:02 +0300

    Solve exercise 1.3 in F#

---
+++ fsharp/chapter1/Exercise_1_3.fs
@@ -0,0 +1,19 @@
+﻿module Exercise_1_3
+
+    
+let public replaceSpacebars (s: char array) length =
+    let lastIndex = length - 1
+    let mutable currentLastIndex = lastIndex
+    
+    for i = lastIndex downto 0 do
+        if s.[i] = ' '
+        then
+            for j = currentLastIndex downto (i + 2) do
+                s.[j + 2] <- s.[j]
+                
+            s.[i] <- '%'
+            s.[i + 1] <- '2'
+            s.[i + 2] <- '0'
+            
+            currentLastIndex <- currentLastIndex + 2
+
--- fsharp/chapter1/Program.fs
+++ fsharp/chapter1/Program.fs
@@ -1,6 +1,6 @@
-﻿open System
-open Exercise_1_1
+﻿open Exercise_1_1
 open Exercise_1_2
+open Exercise_1_3
 
 // 1.1
 [<EntryPoint>]
@@ -13,6 +13,12 @@ let main argv =
     printfn "1.2 isPermutation \"\" \"\" -> %b" (isPermutation "" "")
     printfn "1.2 isPermutation \"abc\" \"bac\" -> %b" (isPermutation "abc" "bac")
     printfn "1.2 isPermutation \"bar\" \"baz\" -> %b" (isPermutation "bar" "baz")
+    printfn ""
+    
+    let v = [|'M';  'r';  ' ';  'J';  'o'; 'h'; 'n'; ' '; 'S'; 'm'; 'i'; 't'; 'h'; ' '; ' '; ' '; ' ' |]
+    printfn "1.3 Before replaceSpacebars: %A" v
+    replaceSpacebars v 13
+    printfn "1.3 After prelaceSpacebars: %A" v
     printfn ""
     
     0
