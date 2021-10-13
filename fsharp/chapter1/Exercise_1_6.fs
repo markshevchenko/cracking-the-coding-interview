dc92406a575bf470ebc50d4362940b64c47642cb
Mark Shevchenko <markshevchenko@gmail.com>
2021-10-13 11:24:48 +0300

    Solve exercise 1.6 in F#

---
+++ fsharp/chapter1/Exercise_1_6.fs
@@ -0,0 +1,38 @@
+﻿module Exercise_1_6
+
+open System.Text
+
+
+let internal appendCharAndCount (stringBuilder: StringBuilder) (c: char) (count: int) =
+    stringBuilder.Append(c)
+                 .Append(count.ToString())
+               |> ignore
+
+
+let internal rle (s: string) =
+    let mutable count = 0
+    let mutable prev = '\000'
+    let stringBuilder = new StringBuilder()
+    
+    for c in s do
+        if count = 0 then
+            prev <- c
+            count <- 1
+        else if c = prev then
+            count <- count + 1
+        else
+            appendCharAndCount stringBuilder prev count
+            prev <- c
+            count <- 1
+    
+    if count > 0 then
+        appendCharAndCount stringBuilder prev count
+            
+    stringBuilder.ToString()
+
+
+let public pack source =
+    let packed = rle source
+    if packed.Length < source.Length
+    then packed
+    else source
---
+++ fsharp/chapter1_tests/Exercise_1_6_tests.fs
@@ -0,0 +1,34 @@
+﻿module Exercise_1_6_tests
+
+open System.Text
+open Xunit
+open Exercise_1_6
+
+
+[<Fact>]
+let ``appendCharAndCount - with empty string a 3 - returns a3`` () =
+    let stringBuilder = new StringBuilder()
+    
+    appendCharAndCount stringBuilder 'a' 3
+    
+    Assert.Equal("a3", stringBuilder.ToString())
+
+
+[<Fact>]
+let ``rle - with empty string - returns empty string`` () =
+    Assert.Equal("", rle "")
+
+
+[<Fact>]
+let ``rle - with aabcccccaaa - returns a2b1c5a3`` () =
+    Assert.Equal("a2b1c5a3", rle "aabcccccaaa")
+
+
+[<Fact>]
+let ``pack - with abc - returns abc`` () =
+    Assert.Equal("abc", pack "abc")
+    
+    
+[<Fact>]
+let ``pack - with aabcccccaaa - returns a2b1c5a3`` () =
+    Assert.Equal("a2b1c5a3", pack "aabcccccaaa")
