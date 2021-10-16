c5a54628e18908c6fa5f7d7006639e923b4f6ede
Mark Shevchenko <markshevchenko@gmail.com>
2021-10-16 09:29:11 +0300

    Solve exercise 1.8 in F#

---
+++ fsharp/chapter1/Exercise_1_8.fs
@@ -0,0 +1,22 @@
+﻿module Exercise_1_8
+
+open System.Collections.Generic
+
+let public zeroColumnsRows (matrix: int[,]) =
+    let zeroRows = HashSet<int>()
+    let zeroColumns = HashSet<int>()
+    
+    for i = 0 to matrix.GetLength(0) - 1 do
+        for j = 0 to matrix.GetLength(1) - 1 do
+            if matrix.[i, j] = 0 then
+                zeroColumns.Add(j) |> ignore
+                zeroRows.Add(i) |> ignore
+                
+    for row in zeroRows do
+        for j = 0 to matrix.GetLength(1) - 1 do
+            matrix.[row, j] <- 0
+            
+    for column in zeroColumns do
+        for i = 0 to matrix.GetLength(0) - 1 do
+            matrix.[i, column] <- 0
+    
---
+++ fsharp/chapter1_tests/Exercise_1_8_tests.fs
@@ -0,0 +1,46 @@
+﻿module Exercise_1_8_tests
+
+open Xunit
+open Exercise_1_8
+
+
+[<Fact>]
+let ``zeroColumnsRows - without zeros - returns same matrix`` () =
+    let matrix = array2D [
+        [11; 12; 13; 14; 15; 16];
+        [21; 22; 23; 24; 25; 26];
+        [31; 32; 33; 34; 35; 36];
+        [41; 42; 43; 44; 45; 46];
+    ]
+    
+    let expected = array2D [
+        [11; 12; 13; 14; 15; 16];
+        [21; 22; 23; 24; 25; 26];
+        [31; 32; 33; 34; 35; 36];
+        [41; 42; 43; 44; 45; 46];
+    ]
+    
+    zeroColumnsRows matrix
+    
+    Assert.Equal<int[,]>(expected, matrix)
+    
+    
+[<Fact>]
+let ``zeroColumnsRows - with zero - sets zeros in column and row`` () =
+    let matrix = array2D [
+        [11; 12; 13; 14; 15; 16];
+        [21; 22; 23;  0; 25; 26];
+        [31; 32; 33; 34; 35; 36];
+        [41; 42; 43; 44; 45; 46];
+    ]
+    
+    let expected = array2D [
+        [11; 12; 13;  0; 15; 16];
+        [ 0;  0;  0;  0;  0;  0];
+        [31; 32; 33;  0; 35; 36];
+        [41; 42; 43;  0; 45; 46];
+    ]
+    
+    zeroColumnsRows matrix
+    
+    Assert.Equal<int[,]>(expected, matrix)
