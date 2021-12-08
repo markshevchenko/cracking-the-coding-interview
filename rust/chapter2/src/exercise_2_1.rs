2d396234410d061abedbd72d321e644becdc96df
Mark Shevchenko <mark-progmsk@yandex-team.ru>
2021-12-08 12:41:50 +0300

    Solve exercise 2.1 additional in Rust

--- rust/chapter2/src/exercise_2_1.rs
+++ rust/chapter2/src/exercise_2_1.rs
@@ -6,7 +6,7 @@
  */
 
 use std::collections::HashSet;
-use super::linked_list::{List};
+use super::linked_list::{List, Node};
 
 pub fn remove_duplicates(list: &mut List) {
     let mut current = match list.head.as_mut() {
@@ -47,4 +47,47 @@ fn remove_duplicates_with121341356_returns123456() {
     remove_duplicates(&mut list);
 
     assert_eq!(expected, list);
+}
+
+fn is_in_list(value: i32, node: &Node) -> bool {
+    if value == node.value {
+        true
+    } else {
+        if let Some(next_node) = &node.next {
+            is_in_list(value, next_node)
+        } else {
+            false
+        }
+    }
+}
+
+fn recursive_remove_duplicates(node: &Node) -> Node {
+    match &node.next {
+        None => Node { value: node.value, next: None },
+        Some(next_node) => if is_in_list(node.value, next_node) {
+            recursive_remove_duplicates(next_node)
+        } else {
+            Node {
+                value: node.value,
+                next: Some(Box::new(recursive_remove_duplicates(next_node)))
+            }
+        }
+    }
+}
+
+pub fn remove_duplicates2(list: &List) -> List {
+    match &list.head {
+        None => List { head: None },
+        Some(node) => List { head: Some(Box::new(recursive_remove_duplicates(node))) },
+    }
+}
+
+#[test]
+fn remove_duplicates2_with121341356_returns123456() {
+    let list = List::from_vec(&vec![1, 2, 1, 3, 4, 1, 3, 5, 6]);
+    let expected = List::from_vec(&vec![2, 4, 1, 3, 5, 6]);
+
+    let actual = remove_duplicates2(&list);
+
+    assert_eq!(expected, actual);
 }
--- rust/chapter2/src/main.rs
+++ rust/chapter2/src/main.rs
@@ -10,4 +10,12 @@ fn main() {
         exercise_2_1::remove_duplicates(&mut list);
         println!("List after remove_duplicates: {}", list);
     }
+    {
+        // Exercise 2.1 — alternative
+        let before = linked_list::List::from_vec(&vec![1, 2, 1, 3, 4, 1, 3, 5, 6]);
+
+        println!("List before remove_duplicates: {}", before);
+        let after = exercise_2_1::remove_duplicates2(&before);
+        println!("List after remove_duplicates: {}", after);
+    }
 }
