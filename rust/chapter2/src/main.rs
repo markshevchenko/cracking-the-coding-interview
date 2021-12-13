b160b0e2f868468e9bf102c92b50e733fb90a435
Mark Shevchenko <mark-progmsk@yandex-team.ru>
2021-12-13 18:10:13 +0300

    Add call to main function

--- rust/chapter2/src/main.rs
+++ rust/chapter2/src/main.rs
@@ -1,3 +1,5 @@
+use crate::exercise_2_2::find_kth_from_end;
+
 mod exercise_2_1;
 mod exercise_2_1a;
 mod exercise_2_2;
@@ -9,16 +11,22 @@ fn main() {
         // Exercise 2.1
         let mut list = mutable_list::List::from_vec(&vec![1, 2, 1, 3, 4, 1, 3, 5, 6]);
 
-        println!("List before remove_duplicates: {}", list);
+        println!("2.1 List before remove_duplicates: {}", list);
         exercise_2_1::remove_duplicates(&mut list);
-        println!("List after remove_duplicates: {}", list);
+        println!("    List after remove_duplicates: {}", list);
     }
     {
         // Exercise 2.1a
         let before = immutable_list::List::from_vec(&vec![1, 2, 1, 3, 4, 1, 3, 5, 6]);
 
-        println!("List before remove_duplicates: {}", before);
+        println!("2.1a List before remove_duplicates: {}", before);
         let after = exercise_2_1a::remove_duplicates(&before);
-        println!("List after remove_duplicates: {}", after);
+        println!("     List after remove_duplicates: {}", after);
+    }
+    {
+        // Exercise 2.2
+        let list = immutable_list::List::from_vec(&vec![1, 2, 3, 4, 5, 6]);
+
+        println!("2.2 find_kth_from_end({}, 2) is {:?}", &list, find_kth_from_end(&list, 2));
     }
 }
